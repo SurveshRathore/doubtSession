d0a8e869e55db60c10077bced4cf2c256721eb5a
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-05-26 17:54:38 +0530

    Create jsArrowFunction.js

---
+++ JavaScript/jsArrowFunction.js
@@ -0,0 +1,11 @@
+// simple function
+var x = function(x, y) {
+    return x * y;
+  }
+
+  console.log("simple function", x(4,5));
+  
+  // arrow function
+  const arrowX = (x, y) => x * y;
+
+  console.log("arrow function", arrowX(4,5));
